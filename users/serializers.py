f31ccefcdbc3eac0cec068c3fc0756582e79abc1
Дмитрий С <112376316+DSvistelnik@users.noreply.github.com>
2023-03-13 10:01:57 +0300

    users

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 from rest_framework.fields import IntegerField
-from rest_framework.relations import SlugRelatedField
-from rest_framework.serializers import ModelSerializer
 
 from users.models import Location, User
 
@@ -37,27 +35,30 @@ class UserDetailSerializer(serializers.ModelSerializer):
         exclude = ["password"]
 
 
-class UserCreateSerializer(ModelSerializer):
-    location = SlugRelatedField(required=False, queryset=Location.objects.all(), slug_field="name", many=True)
-
-    def is_valid(self, *, raise_exception=False):
-        self._locations = self.initial_data.pop("locations", [])
-        return super().is_valid(raise_exception=raise_exception)
-
-    def create(self, validated_data):
-        password = validated_data.pop("password")
-        new_user = User.objects.create(**validated_data)
-        new_user.set_password(password)
-        new_user.save()
-        for loc in self._locations:
-            location, _ = Location.objects.get_or_create(name=loc)
-            new_user.locations.add(location)
-        return new_user
+class UserCreateSerializer(serializers.ModelSerializer):
+    locations = serializers.SlugRelatedField(
+        many=True,
+        required=False,
+        queryset=Location.objects.all(),
+        slug_field="name"
+    )
 
     class Meta:
         model = User
         fields = "__all__"
 
+    def is_valid(self, *, raise_exception=False):
+        self._locations = self.initial_data.pop("locations")
+        return super().is_valid(raise_exception=raise_exception)
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data["password"])
+        for location in self._locations:
+            location_obj, created = Location.objects.get_or_create(name=location)
+            user.locations.add(location_obj)
+        user.save()
+        return user
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,9 @@
 from users.views import *
-from django.contrib import admin
+
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework import routers
-from users.views import LocationViewSet
 
-location_router = routers.SimpleRouter()
-location_router.register("location", LocationViewSet)
+
 
 urlpatterns = [
     path('', UserListView.as_view()),
@@ -20,5 +17,3 @@ urlpatterns = [
 
 
 ]
-
-urlpatterns += location_router.urls
